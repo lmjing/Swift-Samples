446baa066a5502927e6b51bdcf82069773a2f9da
Tacademy-Mac <tacademy-mac@Tacademy-Macui-MacBook-Pro.local>
2016-12-21 11:04:16 +0900

    struct 예제 추가

--- 08.StructAndEnum/struct.swift
+++ 08.StructAndEnum/struct.swift
@@ -29,3 +29,13 @@ struct Point2 {
 var obj3 = Point2()
 // Error : Initializer를 작성했기 때문에 자동 생성 안됨
 // var obj4 = Point2(x : 10, y : 10)
+
+
+struct Point3 {
+    var x : Int
+    var y : Int
+}
+
+// Initializer도 없고 값 초기화가 안되어 있을 경우 아래코드 실행 불가
+//var obj5 = Point3()
+var obj6 = Point3(x: 1, y: 1)
